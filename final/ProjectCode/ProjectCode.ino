7942a31ff9b4df390811498885a803e9b12003c9
Andrew Tsui <tiktiktsui@gmail.com>
2020-11-30 17:42:46 -0800

    update of the final code

--- final/ProjectCode/ProjectCode.ino
+++ final/ProjectCode/ProjectCode.ino
@@ -44,12 +44,10 @@ void loop() {
   else {
     for (pos = pos; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
-                             // waits 15ms for the servo to reach the position
     }
     for (pos2 = pos2; pos2 <= 180; pos2 += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       myservo2.write(pos2);              // tell servo to go to position in variable 'pos'
-      // waits 15ms for the servo to reach the position
     }
   }
 }
